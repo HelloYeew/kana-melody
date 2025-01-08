5389be5e9274dc37e51534e01689fe5172f4ec06
Phawit Pornwattanakul <me@helloyeew.dev>
2025-01-08 15:50:11 +0700

    Click to open full album

--- Components/PlaybackController.axaml.cs
+++ Components/PlaybackController.axaml.cs
@@ -1,10 +1,14 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace KanaMelody.Components;
 
 public partial class PlaybackController : UserControl
 {
+    private Window? _pictureWindow;
+    
     public PlaybackController()
     {
         InitializeComponent();
@@ -13,5 +17,29 @@ public partial class PlaybackController : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+    
+    private void Image_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (_pictureWindow is not null)
+        {
+            _pictureWindow.Focus();
+            return;
+        }
+        if (sender is Image image && image.Source is not null)
+        {
+            _pictureWindow = new Window
+            {
+                Content = new Image
+                {
+                    Source = image.Source,
+                    Stretch = Avalonia.Media.Stretch.Uniform
+                },
+                Width = 800,
+                Height = 600
+            };
+            _pictureWindow.Show();
+            _pictureWindow.Closed += (s, e) => _pictureWindow = null;
+        }
     }
 }
